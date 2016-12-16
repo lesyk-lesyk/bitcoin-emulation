977e3273e624330bd20f3d193e491415b4f91259
Oleksiy Kachynskyy <oleksiy.kachynskyy@gmail.com>
2016-12-16 04:28:32 +0200

    Rename file for task3, remove unnecessary files.

--- 2-task.py
+++ 2-task.py
@@ -1,6 +1,5 @@
 import math
 import matplotlib.pyplot as plot
-from plotEC import plotEC
 from coord import Coord
 
 # Set Dimention
--- plotEC.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def plotEC(plt, a=5, b=4, p=10):
-    y, x = np.ogrid[-p:p:100j, -p:p:100j]
-    plt.contour(x.ravel(), y.ravel(), pow(y, 2) - pow(x, 3) - x * a - b, [0])
-    plt.grid()
-
-if __name__ == '__main__':
-    plotEC()
